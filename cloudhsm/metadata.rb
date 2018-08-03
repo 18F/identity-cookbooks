825767bdd051661e9d64956745b52fbda6e736c7
Andy Brody <andy.brody@beta.dhs.gov>
2018-08-03 15:17:31 -0400

    Add a cookbook for configuring CloudHSM clients.

---
+++ cloudhsm/metadata.rb
@@ -0,0 +1,11 @@
+name 'cloudhsm'
+maintainer 'The Login.gov Team'
+maintainer_email 'identity-devops@login.gov'
+license 'CC0-1.0'
+description 'AWS CloudHSM config cookbook'
+long_description 'Resources and recipes for discovering CloudHSM clusters and setting up CloudHSM client libraries'
+version '0.0.3'
+chef_version '>= 13.0' if respond_to?(:chef_version)
+
+issues_url 'https://github.com/18F/identity-cookbooks/issues'
+source_url 'https://github.com/18F/identity-cookbooks'
