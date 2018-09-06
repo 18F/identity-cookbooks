8878ce3181d1938036c92e6b533acc4cdc9ca5fa
Mark Ryan <mark-from-usds@users.noreply.github.com>
2018-09-06 14:53:39 -0400

    Add a working test

--- passenger/spec/default_spec.rb
+++ passenger/spec/default_spec.rb
@@ -6,6 +6,7 @@ describe 'passenger::daemon' do
     ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '16.04') do |node|
       node.normal['cloud']['local_ipv4'] = '1.2.3.4'
       node.normal['identity-ruby']['rbenv_root'] = '/opt/ruby_build'
+      stub_command("test -e /opt/nginx/sbin/nginx").and_return(0)
     end.converge(described_recipe)
   }
   it 'creates nginx.conf' do
