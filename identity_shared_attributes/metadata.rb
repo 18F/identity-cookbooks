ce5b742c859ee81ce1aaff705437a4eafccbf8f0
Mark Ryan <mark-from-usds@users.noreply.github.com>
2018-09-06 17:11:14 -0400

    Fix description

--- identity_shared_attributes/metadata.rb
+++ identity_shared_attributes/metadata.rb
@@ -2,7 +2,7 @@ name 'identity_shared_attributes'
 maintainer 'The Login.gov Team'
 maintainer_email 'identity-devops@login.gov'
 license 'CC0-1.0'
-description 'NTP setup for login.gov'
+description 'shared attributes for login.gov'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 chef_version '>= 13.0' if respond_to?(:chef_version)
