94c972e58cff7ac2bd1a4648eba620568c451e81
Mark Ryan <34074974+mark-from-usds@users.noreply.github.com>
2018-09-06 17:27:11 -0400

    Create identity_shared_attributes for things that span recipes/repos (#4)

---
+++ identity_shared_attributes/attributes/default.rb
@@ -0,0 +1,3 @@
+default[:identity_shared_attributes][:cache_dir] = '/var/cache/chef'
+default[:identity_shared_attributes][:openssl_version] = '1.0.2o'
+default[:identity_shared_attributes][:production_user] = 'websrv'
