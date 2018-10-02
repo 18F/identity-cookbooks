e5bdcd9adeff604954ddc638e9561c8a70a5be18
Mark Ryan <mark-from-usds@users.noreply.github.com>
2018-10-02 15:08:06 -0400

    Add system_user to identity_shared_attributes

--- identity_shared_attributes/attributes/default.rb
+++ identity_shared_attributes/attributes/default.rb
@@ -1,3 +1,4 @@
 default[:identity_shared_attributes][:cache_dir] = '/var/cache/chef'
 default[:identity_shared_attributes][:openssl_version] = '1.0.2o'
 default[:identity_shared_attributes][:production_user] = 'websrv'
+default[:identity_shared_attributes][:system_user] = 'appinstall'
