140269fe126b42fda6a4796faa57062988da7895
Andy Brody <andy.brody@usds.dhs.gov>
2019-10-18 12:45:45 -0400

    Add a helper script to skip the ASG abandon hook.
    When troubleshooting issues with instance provisioning, it is often
    useful to keep an instance around for inspection. We have a flag file
    that we can drop in /etc/login.gov/info/ that instructs provision.sh not
    to complete the ASG lifecycle hook with ABANDON in the event that
    provisioning fails.
    
    But it's hard to remember this filename when you're racing the clock to
    save the instance.
    
    Add a new script, `id-asg-skip-abandon-hook`, that knows the name of
    this file and will create it for you.

--- identity_base_config/recipes/default.rb
+++ identity_base_config/recipes/default.rb
@@ -50,6 +50,13 @@ cookbook_file '/usr/local/bin/id-apt-upgrade' do
   mode '0755'
 end
 
+cookbook_file '/usr/local/bin/id-asg-skip-abandon-hook' do
+  source 'id-asg-skip-abandon-hook'
+  owner 'root'
+  group 'root'
+  mode '0755'
+end
+
 cookbook_file '/usr/local/bin/id-chef-client' do
   source 'id-chef-client'
   owner 'root'
