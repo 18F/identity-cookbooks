6cf3ef61f5df0c630c9a1750f4c7bdbd98f5cb97
Brian Crissup <crissupb@amazon.com>
2019-10-30 08:49:43 -0400

    Update shared attribute version

--- identity_fips_openssl/metadata.rb
+++ identity_fips_openssl/metadata.rb
@@ -7,7 +7,7 @@ long_description 'Compiles and installs the OpenSSL FIPS module'
 version '0.2.0'
 chef_version '>= 13.0' if respond_to?(:chef_version)
 
-depends 'identity_shared_attributes', '~> 0.2'
+depends 'identity_shared_attributes', '>= 0.1.2'
 
 # The `source_url` points to the development repository for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
