336174ce46e4c7f4772a114194a07baec0d9d468
Brian Crissup <crissupb@amazon.com>
2019-10-30 08:17:24 -0400

    Move openssl from base image

---
+++ identity_fips_openssl/attributes/default.rb
@@ -0,0 +1,9 @@
+default['identity_fips_openssl']['cache_dir']                  = default.fetch(:identity_shared_attributes).fetch(:cache_dir) # /var/cache/chef
+default['identity_fips_openssl']['fips']['version']            = '2.0.16'
+default['identity_fips_openssl']['fips']['url']                = "https://www.openssl.org/source/openssl-fips-#{default['identity_fips_openssl']['fips']['version']}.tar.gz"
+default['identity_fips_openssl']['fips']['checksum']           = 'a3cd13d0521d22dd939063d3b4a0d4ce24494374b91408a05bdaca8b681c63d4'
+default['identity_fips_openssl']['openssl']['version']         = '1.0.2s'
+default['identity_fips_openssl']['openssl']['prefix']          = "/opt/openssl-#{default['identity_fips_openssl']['openssl']['version']}"
+default['identity_fips_openssl']['openssl']['url']             = "https://www.openssl.org/source/openssl-#{default['identity_fips_openssl']['openssl']['version']}.tar.gz"
+default['identity_fips_openssl']['openssl']['checksum']        = 'cabd5c9492825ce5bd23f3c3aeed6a97f8142f606d893df216411f07d1abab96'
+default['identity_fips_openssl']['openssl']['configure_flags'] = %W[ shared ]
