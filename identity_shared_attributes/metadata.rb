73bde8c17e83f741d858d5e8570663b155f20060
bleachbyte <jonathan.pirro@gsa.gov>
2019-11-07 18:57:33 -0800

    Revert "updating cookbook version"
    This reverts commit c54d0a6b4d74e37699ebc0458555c80d80a97f2b.

--- identity_shared_attributes/metadata.rb
+++ identity_shared_attributes/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'identity-devops@login.gov'
 license 'CC0-1.0'
 description 'shared attributes for login.gov'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.3'
+version          '0.1.2'
 chef_version '>= 13.0' if respond_to?(:chef_version)
 
 issues_url 'https://github.com/18F/identity-cookbooks/issues'
