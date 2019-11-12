19795361472e8e9e45bab998a8546f8db94b79ca
bleachbyte <jonathan.pirro@gsa.gov>
2019-11-12 11:49:44 -0800

    putting values in files / reverting back to file

--- identity_base_config/recipes/default.rb
+++ identity_base_config/recipes/default.rb
@@ -64,16 +64,24 @@ cookbook_file '/usr/local/bin/id-chef-client' do
   mode '0755'
 end
 
-template "/usr/local/bin/id-rails-console" do
-    source "id-rails-console.erb"
-    owner 'root'
-    group 'root'
-    mode '0755'
-    variables ({
-      :webhook => ConfigLoader.load_config(node, "slackwebhook"),
-      :channel => ConfigLoader.load_config(node, "slackchannel"),
-    })
+# download Slack hook/channel files for id-rails-console
+aws_account_id = AwsMetadata.get_aws_account_id
+
+s3_secret_url = "s3://login-gov.secrets.#{aws_account_id}-us-west-2/#{node.chef_environment}/"
+
+%w(slackwebhook slackchannel).each do |f|
+  execute "download #{f} from s3" do
+    command "aws s3 cp #{s3_secret_url}#{f} /etc/login.gov/keys/"
+    not_if { ::File.exist?("/etc/login.gov/keys/#{f}") }
   end
+end
+
+cookbook_file '/usr/local/bin/id-rails-console' do
+  source 'id-rails-console'
+  owner 'root'
+  group 'root'
+  mode '0755'
+end
 
 cookbook_file '/usr/local/bin/git-with-key' do
   source 'git-with-key'
