33381ff1bdd2d345cf213d765acc773b099dfc8b
Brian Crissup <brian@BigMac.local>
2020-04-15 10:59:37 -0400

    Force ruby_build to 1.2 version

--- identity_ruby/metadata.rb
+++ identity_ruby/metadata.rb
@@ -8,7 +8,7 @@ version '0.2.0'
 chef_version '>= 13.0' if respond_to?(:chef_version)
 
 depends 'identity_shared_attributes', '>= 0.1.2'
-depends 'ruby_build', '~> 1.2'
+depends 'ruby_build', '= 1.2'
 
 # The `source_url` points to the development repository for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
