8c5bfd791f9944c6bcc2b97eda0eeefd8baa5aa0
Brian Crissup <crissupb@amazon.com>
2021-04-20 10:22:55 -0400

    Add V2 token

--- aws_metadata/metadata.rb
+++ aws_metadata/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures aws_metadata'
 long_description 'Installs/Configures aws_metadata'
-version '0.2.0'
+version '0.3.0'
 chef_version '>= 12.15.19' if respond_to?(:chef_version)
 
 # The `issues_url` points to the location where issues for this cookbook are
