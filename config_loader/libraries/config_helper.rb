d2fadbc0be662afe902e70322827effafbf70bf0
Mitchell Henke <mitchell.henke@gsa.gov>
2021-10-01 13:39:33 -0500

    remove citadel

--- config_loader/libraries/config_helper.rb
+++ config_loader/libraries/config_helper.rb
@@ -2,19 +2,33 @@ require 'json'
 
 class Chef::Recipe::ConfigLoader
   def self.load_config(node, key, common: false)
-    citadel = Citadel.new(node)
+    url = "s3://#{node['login_dot_gov']['secrets_bucket']}"
     prefix = common ? "common" : node.chef_environment
-    citadel[File.join(prefix, key)]
+    url = "#{url}/#{prefix}/#{key}"
+    result = `aws s3 cp #{url} -`.chomp
+    if $?.success?
+      result
+    else
+      raise result
+    end
+  rescue StandardError => err
+    if (err.message =~ /An error occurred \(404\)/ &&
+        node.chef_environment != "prod")
+      Chef::Log.warn(err.message) if print_warnings
+      return nil
+    else
+      raise
+    end
   end
 
-  # Like load_config, but return nil with a warning if Citadel receives an HTTP
+  # Like load_config, but return nil with a warning if s3 receives an HTTP
   # 404 response code. This only applies to cases where node.chef_environment
   # is not "prod". In the "prod" environment, always raise on errors and never
   # return nil.
   def self.load_config_or_nil(node, key, print_warnings: true)
-    load_config(node, key)
-  rescue Citadel::CitadelError => err
-    if (err.message =~ /Unable to download .*: 404 "Not Found"/ &&
+    raw = load_config(node, key)
+  rescue err
+    if (err.message =~ /An error occurred \(404\)/ &&
         node.chef_environment != "prod")
       Chef::Log.warn(err.message) if print_warnings
       return nil
@@ -24,11 +38,16 @@ class Chef::Recipe::ConfigLoader
   end
 
   # Like load_config, but designed to handle nested secrets data. If the
-  # contents are being loaded from citadel, also JSON.parse the data.
+  # contents are being loaded, also JSON.parse the data.
   def self.load_json(node, key, common: false)
-    citadel = Citadel.new(node)
+    url = "s3://#{node['login_dot_gov']['secrets_bucket']}"
     prefix = common ? "common" : node.chef_environment
-    raw = citadel[File.join(prefix, key)]
-    JSON.parse(raw)
+    url = "#{url}/#{prefix}/#{key}"
+    raw = `aws s3 cp #{url} - 2>&1`.chomp
+    if $?.success?
+      JSON.parse(raw)
+    else
+      raise raw
+    end
   end
 end
