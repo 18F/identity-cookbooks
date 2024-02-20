054944feba64240508deb9d39d1d7d7cf729bdbf
mitchell.henke <mitchell.henke@gsa.gov>
2024-02-20 21:51:24 +0000

    Remove Passenger/Nginx configuration from Passenger Daemon Recipe

--- passenger/attributes/daemon.rb
+++ passenger/attributes/daemon.rb
@@ -21,51 +21,5 @@ default[:passenger][:production][:configure_flags] = "--with-ipv6 \
 
 default[:passenger][:production][:log_path] = '/var/log/nginx'
 
-# Relevant for any NGINX instance behind an ALB
-default[:passenger][:production][:log_alb_headers] = true
-default[:passenger][:production][:log_alb_ssl] = true
-
-# Relevant for any NGINX instance behind CloudFront with the related
-# extended headers enabled in the Origin Request Policy
-# See: https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/using-cloudfront-headers.html#cloudfront-headers-viewer-location
-default[:passenger][:production][:log_cloudfront_headers] = true
-
-# Relevant only when client certificates are passed directly to NGINX
-default[:passenger][:production][:log_client_ssl] = false
-
 # Enable the status server on 127.0.0.1
 default[:passenger][:production][:status_server] = true
-
-default[:passenger][:production][:user] = node.fetch(:identity_shared_attributes).fetch(:production_user)
-# Adds Cloudfront CIDRS to the set_real_ip_from to allow for proper client ip preservation
-default[:passenger][:production][:enable_cloudfront_support] = true
-
-# Tune the following configurations for your environment. For more info see:
-# https://www.phusionpassenger.com/library/config/nginx/optimization
-default[:passenger][:production][:gzip] = true
-
-# Nginx's default is 0, but we don't want that.
-default[:passenger][:production][:keepalive_timeout] = '60 50'
-
-default[:passenger][:production][:limit_connections] = true
-
-# Keep max_pool_size and min_instances the same to create a fixed process pool
-default[:passenger][:production][:max_pool_size] = node.fetch('cpu').fetch('total') * 2
-default[:passenger][:production][:min_instances] = node.fetch('cpu').fetch('total') * 2
-default[:passenger][:production][:max_request_queue_size] = 512
-
-default[:passenger][:production][:max_instances_per_app] = 0
-default[:passenger][:production][:pool_idle_time] = 0
-
-# a list of URL's to pre-start.
-default[:passenger][:production][:pre_start] = []
-
-default[:passenger][:production][:sendfile] = true
-default[:passenger][:production][:tcp_nopush] = false
-
-# Set worker processes to number of CPU cores until benchmarking shows that we
-# should do otherwise. See:
-# http://nginx.org/en/docs/ngx_core_module.html#worker_processes
-default[:passenger][:production][:worker_processes] = node.fetch('cpu').fetch('total')
-default[:passenger][:production][:worker_connections] = 1024
-default[:passenger][:production][:nofile_limit] =  node.fetch('passenger').fetch('production').fetch('worker_connections') * 2
