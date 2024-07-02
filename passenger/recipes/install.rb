db565c294b0b6d6bd126eafdf44392a38071c30f
mitchell.henke <mitchell.henke@gsa.gov>
2024-07-02 18:13:21 +0000

    Remove Passenger installation and only install nginx

--- passenger/recipes/install.rb
+++ passenger/recipes/install.rb
@@ -2,15 +2,3 @@
 # Cookbook Name:: passenger
 # Recipe:: install
 
-gem_package "passenger/system" do
-  gem_binary (node.fetch(:identity_shared_attributes).fetch(:rbenv_root) + '/shims/gem')
-  package_name 'passenger'
-  version node[:passenger][:production][:version]
-  retries 12
-  retry_delay 5
-  notifies :run, 'execute[rbenv rehash]', :immediately
-end
-
-execute 'rbenv rehash' do
-  action :nothing # notify only
-end
