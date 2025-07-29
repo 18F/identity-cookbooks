7931000ec7c82cfbf5364942aa7e796108db8803
Connor McCarthy <connor.mccarthy@gsa.gov>
2025-07-29 09:15:49 -0400

    Adding detect-secrets to devenv

--- devenv.nix
+++ devenv.nix
@@ -9,7 +9,22 @@
 {
   # https://devenv.sh/packages/
   packages = with pkgs; [
+    detect-secrets
     git
     glab
   ];
+
+  git-hooks.hooks = {
+    detect-secrets = {
+      enable = true;
+      name = "detect-secrets";
+      description = "Detects high entropy strings that are likely to be passwords.";
+      entry = "detect-secrets-hook";
+      language = "python";
+      args = [
+        "--baseline"
+        ".secrets.baseline"
+      ];
+    };
+  };
 }
